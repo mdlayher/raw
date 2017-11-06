d7f7af8f2829ffa4d5ffa3a6d2d0647c65d3a179
Julian Kornberger <jk+github@digineo.de>
2017-11-06 22:02:37 +0100

    Omit time.Duration type

--- raw.go
+++ raw.go
@@ -22,7 +22,7 @@ const (
 
 	// Maximum read timeout per syscall.
 	// It is required because read/recvfrom won't be interrupted on closing of the file descriptor.
-	readTimeout time.Duration = time.Millisecond * 200
+	readTimeout = 200 * time.Millisecond
 )
 
 var (
