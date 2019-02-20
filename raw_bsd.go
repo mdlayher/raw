198500b50ae53d1be0b60edf7ddf4f4089c0ef66
David Hill <dhill@mindcry.org>
2019-02-20 11:01:03 -0500

    Add OpenBSD support.

--- raw_bsd.go
+++ raw_bsd.go
@@ -280,25 +280,6 @@ func configureBPF(fd int, ifi *net.Interface, proto uint16) (int, error) {
 	return buflen, nil
 }
 
-// setBPFDirection enables filtering traffic traveling in a specific direction
-// using BPF, so that traffic sent by this package is not captured when reading
-// using this package.
-func setBPFDirection(fd int, direction int) error {
-	_, _, err := syscall.Syscall(
-		syscall.SYS_IOCTL,
-		uintptr(fd),
-		// Even though BIOCSDIRECTION is preferred on FreeBSD, BIOCSSEESENT continues
-		// to work, and is required for other BSD platforms
-		syscall.BIOCSSEESENT,
-		uintptr(unsafe.Pointer(&direction)),
-	)
-	if err != 0 {
-		return syscall.Errno(err)
-	}
-
-	return nil
-}
-
 // assembleBpfInsn assembles a slice of bpf.RawInstructions to the format required by
 // package syscall.
 func assembleBpfInsn(filter []bpf.RawInstruction) []syscall.BpfInsn {
